72fd0eadf63c712c7973b30bf99886fadf3720c4
Salisu Shuaibu <salisshuaibu11@gmail.com>
2022-12-14 15:08:38 +0100

    Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,18 +5,17 @@ import { Container } from "./Container"
 export default function Hero() {
   return (
     <div className="relative pt-10 pb-20 sm:py-24">
-      <div className="absolute inset-x-0 -top-48 -bottom-14 overflow-hidden bg-indigo-50">
-        {/* <Image
-          className="absolute top-0 left-0 translate-y-[-10%] translate-x-[-55%] -scale-x-100 sm:left-1/2 sm:translate-y-[-6%] sm:translate-x-[-98%] lg:translate-x-[-106%] xl:translate-x-[-122%]"
-          src="/pattern.jpeg"
+      <div className="absolute hero inset-x-0 -top-48 -bottom-14 overflow-hidden lg:block hidden h-auto w-auto">
+        <Image
+          className="absolute w-full object-cover"
+          src="/pattern.png"
           alt=""
           width={100}
           height={1495}
           priority
           unoptimized
-        /> */}
-        <div className="absolute inset-x-0 top-0 h-40 bg-gradient-to-b from-green-50" />
-        <div className="absolute inset-x-0 bottom-0 h-40 bg-gradient-to-t from-white" />
+        />
+        <div className="absolute inset-x-0 top-0 h-full hero opacity-100" />
       </div>
       <Container className="relative">
         <div className="mx-auto max-w-2xl lg:max-w-4xl lg:px-12 text-center">
